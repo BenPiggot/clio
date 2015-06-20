1f58ecb625b5f25d9b34f27cacfbd033e486b574
wbp <wbpiggot@gmail.com>
2015-06-19 19:41:25 -0700

    Fixing minification

--- tasks/config/uglify.js
+++ tasks/config/uglify.js
@@ -9,17 +9,17 @@
  * 		https://github.com/gruntjs/grunt-contrib-uglify
  *
  */
-// module.exports = function(grunt) {
+module.exports = function(grunt) {
 
-// 	grunt.config.set('uglify', {
-//     dist: {
-// 			src: ['.tmp/public/concat/production.js'],
-// 			dest: '.tmp/public/min/production.min.js'
-// 		},
-//     options: {
-//       mangle: false
-//     }
-// 	});
+	grunt.config.set('uglify', {
+    dist: {
+			src: ['.tmp/public/concat/production.js'],
+			dest: '.tmp/public/min/production.min.js'
+		},
+    options: {
+      mangle: false
+    }
+	});
 
-// 	grunt.loadNpmTasks('grunt-contrib-uglify');
-// };
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+};
