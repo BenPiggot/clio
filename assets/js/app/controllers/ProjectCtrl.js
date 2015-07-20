d9b7cf8e56b6007cb68bb63665a2fb0bca555a16
wbp <wbpiggot@gmail.com>
2015-07-19 20:24:23 -0700

    Changing images

--- assets/js/app/controllers/ProjectCtrl.js
+++ assets/js/app/controllers/ProjectCtrl.js
@@ -23,7 +23,7 @@ var imageArray = [
   "http://citelighter-cards.s3.amazonaws.com/p179jerr2r1c2b19so8hjc2q1ev80_18242.jpg",
   "https://globalizingmexico.files.wordpress.com/2012/03/revsoldiers1.jpg",
   "http://i.ytimg.com/vi/5110UES-QzE/maxresdefault.jpg",
-  "http://api.ning.com/files/M8fY2*UDgJxVQ8f9mwuu1gGQ*lGlKRlJHZBllmDgmrrQgaLv5rEtvjcGc51ZBlOBuPm4cRlmdN7fOmxNoczE8RPsh1CIKUBl/cheguevara.jpg",
+  "http://1.bp.blogspot.com/-FCHp-G--fpA/TeUzPQjIP2I/AAAAAAAAKXA/VOQB-CQAx8k/s1600/Che-Guevara-Smoking-A-Cuban-Cigar.png",
   "http://i.telegraph.co.uk/multimedia/archive/02530/thatcher1__1979-do_2530147k.jpg",
   "http://paperboat.studiopod.com/wp-content/uploads/2010/07/great-depression-soup-line-11.jpg",
   "http://upload.wikimedia.org/wikipedia/en/9/9a/Oxcart-train1947.jpg",
